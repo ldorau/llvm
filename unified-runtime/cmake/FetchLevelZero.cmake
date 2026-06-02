4a79111609e1a1ee61b91769c1f4c4a13bca6d0b
Dorau, Lukasz <lukasz.dorau@intel.com>
2026-06-02 09:51:26 +0000

    [HACK_FOR_TESTING] Update Compute Runtime tag to ci-neo-038554@ldorau

--- unified-runtime/cmake/FetchLevelZero.cmake
+++ unified-runtime/cmake/FetchLevelZero.cmake
@@ -128,8 +128,8 @@ if(L0_COMPUTE_RUNTIME_HEADERS)
     set(COMPUTE_RUNTIME_LEVEL_ZERO_INCLUDE "${L0_COMPUTE_RUNTIME_HEADERS}")
     set(COMPUTE_RUNTIME_REPO_PATH "${L0_COMPUTE_RUNTIME_HEADERS}")
 else()
-    set(UR_COMPUTE_RUNTIME_REPO "https://github.com/intel/compute-runtime.git")
-    set(UR_COMPUTE_RUNTIME_TAG 26.05.37020.3)
+    set(UR_COMPUTE_RUNTIME_REPO "https://github.com/ldorau/compute-runtime.git")
+    set(UR_COMPUTE_RUNTIME_TAG ci-neo-038554)
 
     include(FetchContent)
     # Sparse fetch only the dir with level zero headers for experimental features to avoid pulling in the entire compute-runtime.
